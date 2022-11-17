2ca22c7f9f8e9fd8e539800e2676799102cf432d
saikumar9949 <108319978+saikumar9949@users.noreply.github.com>
2022-11-17 18:50:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,45 @@
 const todoList = () => {
-  all = []
+  all = [];
   const add = (todoItem) => {
-    all.push(todoItem)
-  }
+    all.push(todoItem);
+  };
   const markAsComplete = (index) => {
-    all[index].completed = true
-  }
+    all[index].completed = true;
+  };
 
   const overdue = () => {
-    return all.filter((item)=> {
-      return item.dueDate <= yesterday && item.completed === false
-    })
-  }
+    return all.filter((todo) => todo.dueDate < today);
+  };
 
   const dueToday = () => {
-      return all.filter((item)=> {
-          return item.dueDate === today 
-        })
-  }
+    return all.filter((todo) => todo.dueDate == today);
+  };
 
   const dueLater = () => {
-      return all.filter((item)=> {
-          return item.dueDate >= tomorrow 
-        })
-  }
+    return all.filter((todo) => todo.dueDate > today);
+  };
 
   const toDisplayableList = (list) => {
-    let mylist = [];
-      list.forEach((item) => {
-          if (item.dueDate === today) {
-              if (item.completed === true) {
-                  mylist.push(`[x] ${item.title}`)
-              } else {
-                  mylist.push(`[ ] ${item.title}`)
-              }
-          } else {
-              if (item.completed === true) {
-                  mylist.push(`[x] ${item.title} ${item.dueDate}`)
-              }else{
-                  mylist.push(`[ ] ${item.title} ${item.dueDate}`)
-              }
-          }
-      })
-      return mylist.join("\n")
-  
-  }
+    return list
+      .map(
+        (todo) =>
+          `${todo.completed ? "[x]" : "[ ]"} ${todo.title} ${
+            todo.dueDate == today ? "" : todo.dueDate
+          }`
+      )
+      .join("\n");
+  };
 
-  return { all, add, markAsComplete, overdue, dueToday, dueLater, toDisplayableList };
-}
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
+};
 
 // ####################################### #
 // DO NOT CHANGE ANYTHING BELOW THIS LINE. #
