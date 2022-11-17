eac2a50592716c527f011c132cb0170646ff5a6e
saikumar9949 <108319978+saikumar9949@users.noreply.github.com>
2022-11-17 19:07:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,53 @@
 const todoList = () => {
-  all = [];
+  all = []
   const add = (todoItem) => {
-    all.push(todoItem);
-  };
+    all.push(todoItem)
+  }
   const markAsComplete = (index) => {
-    all[index].completed = true;
-  };
+    all[index].completed = true
+  }
 
   const overdue = () => {
-    return all.filter((todo) => todo.dueDate < today);
-  };
+    return all.filter((item)=> {
+      return item.dueDate <= yesterday && item.completed === false
+    })
+  }
 
   const dueToday = () => {
-    return all.filter((todo) => todo.dueDate == today);
-  };
+      return all.filter((item)=> {
+          return item.dueDate === today 
+        })
+  }
 
   const dueLater = () => {
-    return all.filter((todo) => todo.dueDate > today);
-  };
+      return all.filter((item)=> {
+          return item.dueDate >= tomorrow 
+        })
+  }
 
   const toDisplayableList = (list) => {
-    return list
-      .map(
-        (todo) =>
-          `${todo.completed ? "[x]" : "[ ]"} ${todo.title} ${
-            todo.dueDate == today ? "" : todo.dueDate
-          }`
-      )
-      .join("\n");
-  };
+    let mylist = [];
+      list.forEach((item) => {
+          if (item.dueDate === today) {
+              if (item.completed === true) {
+                  mylist.push(`[x] ${item.title}`)
+              } else {
+                  mylist.push(`[ ] ${item.title}`)
+              }
+          } else {
+              if (item.completed === true) {
+                  mylist.push(`[x] ${item.title} ${item.dueDate}`)
+              }else{
+                  mylist.push(`[ ] ${item.title} ${item.dueDate}`)
+              }
+          }
+      })
+      return mylist.join("\n")
+  
+  }
 
-  return {
-    all,
-    add,
-    markAsComplete,
-    overdue,
-    dueToday,
-    dueLater,
-    toDisplayableList,
-  };
-};
+  return { all, add, markAsComplete, overdue, dueToday, dueLater, toDisplayableList };
+}
 
 // ####################################### #
 // DO NOT CHANGE ANYTHING BELOW THIS LINE. #
